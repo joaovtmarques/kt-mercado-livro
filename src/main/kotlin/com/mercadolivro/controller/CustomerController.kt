8782784957092552273fd69576fe0e00a197b98d
João Vitor Marques <jvsilvam@outlook.com>
2022-09-25 21:33:24 -0300

    add CustomerController and Model

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,15 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.model.CustomerModel
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customers")
+class CustomerController {
+    @GetMapping
+    fun getCustomer(): CustomerModel {
+        return CustomerModel("1", "Joao", "email@email.com")
+    }
+}
