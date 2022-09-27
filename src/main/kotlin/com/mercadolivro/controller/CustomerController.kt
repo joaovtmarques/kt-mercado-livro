41ea5c458878e14a0542f956efa844282546acbc
João Vitor Marques <jvsilvam@outlook.com>
2022-09-27 08:49:37 -0300

    add extension function

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -2,63 +2,44 @@ package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.extension.toCustomerModel
 import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customers")
-class CustomerController {
-
-  val customers = mutableListOf<CustomerModel>()
+class CustomerController(
+  val customerService: CustomerService
+) {
 
   @GetMapping
   fun getAll(@RequestParam name: String?): List<CustomerModel> {
-    name?.let {
-      return customers.filter { it.name.contains(name, true) }
-    }
-    return customers
+    return customerService.getAll(name)
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   fun create(@RequestBody customer: PostCustomerRequest) {
-    val id = if(customers.isEmpty()) {
-      "1"
-    } else {
-      customers.last().id.toInt() + 1
-    }.toString()
-
-    customers.add(CustomerModel(id, customer.name, customer.email))
+    customerService.create(customer.toCustomerModel())
   }
 
   @GetMapping("/{id}")
   fun getCustomer(@PathVariable id: String): CustomerModel {
-    return customers.filter { it.id == id }.first()
+    return customerService.getCustomer(id)
   }
   
   @PutMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
-    customers.filter { it.id == id }.first().let {
-      it.name = customer.name
-      it.email = customer.email
-    }
+    customerService.update(customer.toCustomerModel(id))
   }
   
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   fun update(@PathVariable id: String) {
-    customers.removeIf { it.id == id }
+    customerService.delete(id)
   }
 
 }
