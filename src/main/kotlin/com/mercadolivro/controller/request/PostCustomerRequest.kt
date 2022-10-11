2dc7afcdb77daec72f86fa00bbce7b37cb498021
João Vitor Marques <jvsilvam@outlook.com>
2022-10-11 09:04:12 -0300

    create custom validation

--- src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.controller.request
 
+import com.mercadolivro.validation.EmailAvailable
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotEmpty
 
@@ -8,6 +9,7 @@ data class PostCustomerRequest (
     @field:NotEmpty(message = "Um nome deve ser informado")
     var name: String,
 
+    @EmailAvailable()
     @field:Email(message = "E-mail deve ser válido")
     var email: String
 )
