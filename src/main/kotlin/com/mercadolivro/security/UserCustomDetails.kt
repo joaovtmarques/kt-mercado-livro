89e68cedf2aa2b8b4fedea213f1548ebea93d585
João Vitor Marques <jvsilvam@outlook.com>
2022-10-20 08:38:46 -0300

    create token generation

--- src/main/kotlin/com/mercadolivro/security/UserCustomDetails.kt
+++ src/main/kotlin/com/mercadolivro/security/UserCustomDetails.kt
@@ -7,20 +7,14 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class UserCustomDetails(private val customerModel: CustomerModel): UserDetails {
-	val id = customerModel.id
+	val id: Int = customerModel.id!!
 	
 	override fun getAuthorities(): MutableCollection<out GrantedAuthority> = customerModel.roles.map{ SimpleGrantedAuthority(it.description) }.toMutableList()
-	
 	override fun getPassword(): String = customerModel.password
-	
 	override fun getUsername(): String = customerModel.id.toString()
-	
 	override fun isAccountNonExpired(): Boolean = true
-	
 	override fun isAccountNonLocked(): Boolean = true
-	
 	override fun isCredentialsNonExpired(): Boolean = true
-	
 	override fun isEnabled(): Boolean = customerModel.status == CustomerStatus.ATIVO
 	
 }
