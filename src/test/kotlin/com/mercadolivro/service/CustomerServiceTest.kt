8ee88c5d009068507045e1b4758a02cd75d0ec5d
João Vitor Marques <jvsilvam@outlook.com>
2022-10-28 12:18:14 -0300

    tests customer return when name is informed

--- src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
+++ src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
@@ -48,6 +48,20 @@ class CustomerServiceTest {
 		verify(exactly = 0) { customerRepository.findByNameContaining(any()) }
 	}
 	
+	@Test
+	fun `should return costumers when name is informed`() {
+		val name = UUID.randomUUID().toString()
+		val fakeCustomers = listOf(buildCustomer(), buildCustomer())
+		
+		every { customerRepository.findByNameContaining(name) } returns fakeCustomers
+		
+		val customers = customerService.getAll(name)
+	
+		assertEquals(fakeCustomers, customers)
+		verify(exactly = 0) { customerRepository.findAll() }
+		verify(exactly = 1) { customerRepository.findByNameContaining(name) }
+	}
+	
 	fun buildCustomer(
 		id: Int? = null,
 		name: String = "customer name",
